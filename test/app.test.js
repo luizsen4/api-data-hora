f8f6615dd579095d15db47ac15dc6ee634017155
luizsen4 <luiz34777635@gmail.com>
2026-08-27 15:56:56 -0300

    feat: adição da pasta test + arquivo app.test.js (entendendo como funciona test)

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+// teste/app.test.js
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("API Data e Hora", () => {
+    test("GET / deve retornar HTTP 200", async () => {
+
+        const response = await request(app).get("/");
+
+        expect(response.statusCode).toBe(200);
+    });
+});
